f5406ada904bbf54d77806bd81a13ad121882242
filiprrs <filiprrs@gmail.com>
2019-12-08 13:54:31 +0100

    universal7880: Rename a7y17lte config to unified a57y17lte config
    We can do this because 825ff9850f0fb3c5d4acda4fb0e9c8b8251c728a.
    Looks like a5 one isn't compatible with the Korean base. Get rid of it.
    Reenable dtbtool while we're at it.

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=p
 
-make exynos7880-a7y17ltekor_defconfig
+make exynos7880-a57y17lte_eur_defconfig
 make -j64
 
